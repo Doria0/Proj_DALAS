5a13f1206ff9c35a725dd32431571e8c9133f92a
Doria0 <48170744+Doria0@users.noreply.github.com>
2024-02-25 18:48:13 +0100

    a more robust version:)

--- scrapping_en.py
+++ scrapping_en.py
@@ -7,6 +7,7 @@ import math
 from urllib import request
 import data_utils as util
 import re
+import time
 
 url = "https://en.wikipedia.org/wiki/Spirited_Away"
 
@@ -16,8 +17,11 @@ def get_date(date:str):
   "(YYYY-MM-DD)" --> (YYYY, MM, DD)
   Return: list
   """
-  date_wt_prts = re.search(r"\([\d|-]*\)",date).group(0)
-  return date_wt_prts[1:-1].split('-')
+  date_wt_prts = re.search(r"\([\d|-]*\)",date)
+  if date_wt_prts is not None: #if found
+    date_wt_prts = date_wt_prts.group(0)
+    return date_wt_prts[1:-1].split('-')
+  else: return [math.nan]
 
 def get_running_time(duree:str):
    rt = re.search(r"[\d]* minutes",duree)
@@ -63,18 +67,29 @@ def convert_money(liste_str):
   ou
   "$XX.X-YY.Y XXXXXXXXXX" --> (XXXXXXXX, YYYYYYYYY)
   """
-  values = liste_str[0][1:].split('–')
+#   values = liste_str[0][1:].split('–')
+  values = liste_str[0][1:].split('-')
   if len(values) == 1:
     value_min = math.nan
   else:
-    value_min = float(values[0])
-  value_max = float(values[-1])
-  match liste_str[1]:
-    case "million" : return value_min * (10**6), value_max * (10**6)
-    case "billion" : return value_min * (10**9), value_max * (10**9) # pour les pays anglophones, 1 billion est équivalent au 1 milliard en France
-    case _ : return None
+    vmin = "".join(list(filter(str.isdigit,values[0]))) # can't convert str "123,456" to float directly
+    if vmin != '':
+        value_min = float(vmin)
+    else: value_min = math.nan
+  vmax = "".join(list(filter(str.isdigit,values[-1])))
+  if vmax != '':
+    value_max = float(vmax)
+  else: value_max = math.nan
+  if len(liste_str) >= 2: # if there's a unity written as str for the budget
+    match liste_str[1]:
+        case "million" : return value_min * (10**6), value_max * (10**6)
+        case "billion" : return value_min * (10**9), value_max * (10**9) # pour les pays anglophones, 1 billion est équivalent au 1 milliard en France
+        # case _ : return None  # TypeError: 'NoneType' object is not subscriptable
+  return value_min, value_max
 
 dict_res = {
+    "Tconst du film" : math.nan,
+    "Titre du film" : math.nan,
     "Jour de sortie" : math.nan,
     "Mois de sortie" : math.nan,
     "Année de sortie" : math.nan,
@@ -85,10 +100,11 @@ dict_res = {
     "Box office min" : math.nan,
     "Box office max" : math.nan
 }
-index_list = [0]
+index_list = []
 
 #executable
-def scrap_en(lignes,dict_res):
+def scrap_en(lignes,dict_res,index_list,ind):
+    # index_list[:] = [ind]
     for ligne in lignes:
         ths = ligne.find_all("th")
         for th in ths:
@@ -129,13 +145,15 @@ def scrap_en(lignes,dict_res):
                     duree = get_running_time(duree) # durée (en minutes mais l'unité est toujours la même donc pas besoin de l'enregistrer)
                     dict_res["Durée"] = duree
                 case "Country" :
-                    pays = remove_all_notes(ligne.find("td").text)
-                    dict_res["Pays"] = pays
+                    if ligne.find("td") is not None: # there's some ambiguation, eg:'https://en.wikipedia.org/wiki/Milk'
+                        pays = remove_all_notes(ligne.find("td").text)
+                        dict_res["Pays"] = pays
                 case "Countries" :
-                    pays = remove_all_notes(ligne.find("td").text).split('\n') # sépare les lignes, enlève les notes entre crochets
-                    pays = [elem for elem in pays if elem != ''] # enlever les '' vides
-                    index_list = [i for i in range(len(pays))] # met à jour la liste d'index (pour le dataframe plus tard)
-                    dict_res["Pays"] = pays
+                    if ligne.find("td") is not None:
+                        pays = remove_all_notes(ligne.find("td").text).split('\n') # sépare les lignes, enlève les notes entre crochets
+                        pays = [elem for elem in pays if elem != ''] # enlever les '' vides
+                        index_list[:] = [ind for i in range(len(pays))] # met à jour la liste d'index (pour le dataframe plus tard)
+                        dict_res["Pays"] = pays
                 case "Budget" :
                     budget = remove_all_notes(ligne.find("td").text).split()
                     budget = convert_money(budget) # enlève le $, convertit million/billion dans la valeur
@@ -150,30 +168,49 @@ def scrap_en(lignes,dict_res):
 
 
 if __name__ == '__main__':
-    all_film_titles = util.read_titles("data-2.tsv")[:10]
+    start = time.time()
+    all_film_titles = util.read_titles("data-5.tsv")[:2000] # title.basics
     # traverse all film titles in imdb dataset
     request_texts = []
-    for film_title in all_film_titles:
+    ind = 0 #384/1000, 1100/3000,
+    # index_list = [ind]
+    for (index,film_title) in all_film_titles:
         url_en = util.prefix_en + film_title
         print(f"{url_en=}")
         try:
-            req = request.urlopen(url_en).read()
-            request_texts.append(req)
+            request_text = request.urlopen(url_en).read()
+            dict_res['Tconst du film'] = index
+            dict_res['Titre du film'] = util.rep_udscr_space(film_title)
+            # request_texts.append(req)
             # print(f"{type(req):}") #'bytes'
         except:
-            request_texts.append(math.nan)
-    # print(request_texts[:5])
-    df_all = pd.DataFrame()
-    ind = 0
-    for request_text in request_texts:
+            request_text = math.nan
+            # continue
+        # print(request_texts[:5])
+    # df_all = pd.DataFrame()
+    # for request_text in request_texts:
         # print(f"{request_text=}")
         if request_text is not math.nan:
+            index_list[:] = [ind]
             page = bs4.BeautifulSoup(request_text, "lxml")
             table_info = page.find("table")
-            lignes = table_info.find_all("tr")
-            scrap_en(lignes,dict_res)
-        df = pd.DataFrame(dict_res, index=[ind])
-        df_all = pd.concat([df_all,df])
-        ind += 1
-    # print(df_all[:5])
-    util.save_scrapping('scrapped_en.tsv',df=df_all)
+            if table_info is not None: # if a table exists
+                lignes = table_info.find_all("tr")
+                scrap_en(lignes,dict_res,index_list,ind)
+                print(f"{index_list=}")
+            df = pd.DataFrame(dict_res, index=index_list)
+            if ind==0:
+                df.to_csv('scrapped_en.tsv',sep='\t',na_rep='\\N',mode='a')
+                # write to the file 'scrapped_en.tsv', with seperator \t, representation of nan '\N', 
+                # and append to the file if it exists
+            else:
+                df.to_csv('scrapped_en.tsv',sep='\t',na_rep='\\N',header=False,mode='a') # don't write out column names
+            # df_all = pd.concat([df_all,df])
+            ind += 1
+            #reset dict after the write
+            for key in dict_res:
+               dict_res[key] = math.nan
+        # print(df_all[:5])
+        # util.save_scrapping('scrapped_en.tsv',df=df_all)
+    end = time.time()
+    print("Time Consumed:{} mins".format((end-start)/60.0))
