c86a98795a8f8778cdf4e68f8d0011af13b8ad09
Doria0 <48170744+Doria0@users.noreply.github.com>
2024-02-25 18:49:29 +0100

    encoded some special char in url

--- data_utils.py
+++ data_utils.py
@@ -9,20 +9,24 @@ def read_titles(file):
     """read in from file
     Args:
         file: string, relative or absolute path of the file
-    Return:
-        list, all film titles
+    Returns:
+        tuple, (ind,title with space replaced by _)
     """
     titles = []
     df = pd.read_csv(file,sep='\t')
-    primTitles = df['primaryTitle'].unique() # extract all unique values in the column "primaryTitle"(en)
+    primTitles = df['primaryTitle']
+    tconsts = df['tconst']
     # manner I
-    for pt in primTitles:
+    N = len(primTitles)
+    for i in range(N):
+        pt = primTitles[i]
+        ind = tconsts[i]
         if str(pt).find(' ') > 0:
-            titles.append(rep_space_udscr(pt))
+            titles.append((ind,rep_space_udscr(pt)))
         else:
-            titles.append(pt)
-    # print(f"{titles[:5] = }")
-    # manner II   # how about we just find special cases by hand(?
+            titles.append((ind,pt))
+    print(f"{titles[:5] = }")
+    # manner II
     return titles
 
 def save_scrapping(path,df):
@@ -34,6 +38,16 @@ def rep_space_udscr(s):
     for i in range(len(l)):
         if l[i] == ' ':
             l[i] = '_'
+        if l[i] == '\'':
+            l[i] = '%27'
+    return ''.join(l)
+
+def rep_udscr_space(s):
+    """replace spaces by underscores"""
+    l = list(s)
+    for i in range(len(l)):
+        if l[i] == '_':
+            l[i] = ' '
     return ''.join(l)
 
 # exp request
@@ -41,6 +55,9 @@ prefix_fr = "https://fr.wikipedia.org/wiki/"
 prefix_en = "https://en.wikipedia.org/wiki/"
 film_title_fr = "Le_Voyage_de_Chihiro"
 film_title_en = "Spirited_Away"
+
+## read in names of films from "data-2.tsv"
+
 
 # all_film_titles = read_titles("data-2.tsv")[:10000]
 # traverse all film titles in imdb dataset
