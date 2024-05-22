1bcab6da209a988e736df7563fb183371a6c055b
Doria0 <48170744+Doria0@users.noreply.github.com>
2024-05-22 15:20:06 +0200

    quali var encode and PCA

--- EDA.py
+++ EDA.py
@@ -7,48 +7,84 @@ import os
 
 ## tests
 # df_original = pd.read_csv('test.tsv',sep='\t')# annee/mois/jour sont bien int64
-data_files = ['data/2020-2024_cleaned.tsv']
+data_files_cleaned = ['data/2020-2024_cleaned.tsv']
 save_paths = ['data/2020-2024_budget_boxoffice.tsv']
 
-df_cleaned_all = pd.read_csv('data/2020-2024_cleaned.tsv',sep='\t')
+df_cleaned_all = pd.read_csv('data/2020-2024_cleaned.tsv',sep='\t') # Box-office & Budget don't have missing values
 
-df_budget_boxoffice = util.clean_data(df_cleaned_all, cols_retain=['Budget max','Box office max'])
-df_budget_boxoffice.to_csv('data/2020-2024_budget_boxoffice.tsv',sep='\t')
-# print(df_original.shape)
+# ## Histogramme de Box-office
+# df_budget_boxoffice = util.clean_data(df_cleaned_all, cols_retain=['Budget max','Box office max'])
+# df_budget_boxoffice.to_csv('data/2020-2024_budget_boxoffice.tsv',sep='\t')
+# ax = sns.histplot(data=df_budget_boxoffice['Box office max'].astype('float64').apply(np.log10),bins=200,color='#ffc845')
+# ax.set_xlabel('Box office(log10)')
+# plt.savefig('sources/boxoffice_histo.pdf')
+# plt.show()
 
-# # 对预算和票房进行分桶
-budget_bins = pd.cut(df_budget_boxoffice['Budget max'], bins=500)  # 将预算分为10个区间
-boxoffice_bins = pd.cut(df_budget_boxoffice['Box office max'], bins=500)  # 将票房分为10个区间
+# ## Histogramme de Budget
+# df_budget_boxoffice = pd.read_csv('data/2020-2024_budget_boxoffice.tsv',sep='\t')
+# ax = sns.histplot(data=df_budget_boxoffice['Budget max'].astype('float64').apply(np.log10),bins=200,color='#0cb9c1')
+# ax.set_xlabel('Budget(log10)')
+# plt.savefig('sources/budget_histo.pdf')
+# plt.show()
 
-# # 创建交叉表，计算每个预算区间和票房区间的电影数量
-heatmap_data = pd.crosstab(budget_bins, boxoffice_bins, normalize='index')  # 也可以使用sum或mean
+# # Histogramme de Durée
+# df_duree_boxoffice = util.clean_data(df_cleaned_all, cols_retain=['Durée','Box office max'],cols_na=['Durée'])
+# # print("min duree: {}, max duree: {}".format(df_duree_boxoffice['Durée'].astype('float64').min(), df_duree_boxoffice['Durée'].astype('float64').max()))
+# #16min~305min
+# ax = sns.histplot(data=df_duree_boxoffice['Durée'].astype('float64'),bins=200,color='#037ef3')
+# ax.set_xlabel('Durée/min')
+# plt.savefig('sources/duree_histo.pdf')
+# plt.show()
 
-# # 调整排序，确保区间连续（如果需要）
-heatmap_data = heatmap_data.sort_index(ascending=False)
+# ## scatter
+# # 预算和票房的关系
+# plt.figure(figsize=(12,20),constrained_layout=True) # create a new figure
+# ax = sns.scatterplot(data=df_cleaned_all, x='Budget max', y='Box office max', hue='Pays')#,legend=False)
+# # ax.set_ylim(-0.05e13,0.5e13)
+# ax.set_xscale('log')
+# ax.set_yscale('log')
+# ax.set_xlabel('Budget max',fontsize=10)
+# ax.set_ylabel('Box office max',fontsize=10)
+# # 将图例放在图外并调整大小
+# plt.legend(bbox_to_anchor=(1.02, 1), loc=2, borderaxespad=0., title='Pays',fontsize='small')
+# # plt.savefig('sources/budget_boxoffice_scatter.pdf')
+# plt.show()
 
-plt.figure(figsize=(12, 8))
-sns.heatmap(heatmap_data, annot=True, fmt=".2f")
-plt.title('Heatmap of Movie Budget vs. Boxoffice')
-plt.ylabel('Budget')
-plt.xlabel('Boxoffice')
+## heatmap - Correlation coefficient matrix
+# # variables: boxoffice, budget, duree
+# not very evident
+df_duree_budget_box = util.clean_data(df_cleaned_all, cols_retain=['Tconst du film','Durée','Budget max','Box office max'],cols_na=['Durée'])
+df_duree_budget_box.rename(columns={'Tconst du film': 'tconst'}, inplace=True)
+df_ratings = pd.read_csv('data/imdb/title.ratings.tsv',sep='\t')
+df_dure_budg_rating_box = util.join_data(dfs=[df_duree_budget_box,df_ratings],keys=['tconst'],manners=['left'])
+print(df_dure_budg_rating_box.index)
+# df_dure_budg_rating_box.to_csv('df_dure_budg_rating_box.tsv',sep='\t')
+df_dure_budg_rating_box.drop('tconst', axis=1, inplace=True)
+corr_matrix = df_dure_budg_rating_box.corr()
+plt.figure(figsize=(10, 8))
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".5f", linewidths=.5)
+plt.title('Heatmap of Correlation Matrix')
+plt.savefig('sources/corr_rating_dure_budg_box_heatmap.pdf')
 plt.show()
 
+# budget_bins = pd.cut(df_budget_boxoffice['Budget max'], bins=10)  # 将预算分为10个区间
+# boxoffice_bins = pd.cut(df_budget_boxoffice['Box office max'], bins=10)  # 将票房分为10个区间
 
+# # 创建交叉表，计算每个预算区间和票房区间的电影数量
+# heatmap_data = pd.crosstab(budget_bins, boxoffice_bins, normalize='index')  # 也可以使用sum或mean
 
+# # 调整排序，确保区间连续（如果需要）
+# heatmap_data = heatmap_data.sort_index(ascending=False)
 
-
-
-# # clean missing values and outliers
-# df_all_cleaned = util.clean_data(df_original,cols=['Box office max','Budget max'])
-# print(df_all_cleaned.shape)
-
-# # 票房的直方图
-# sns.histplot(data=df_all_cleaned['Box office max'].astype('float64').apply(np.log),bins=200,color='#ff6050')
+# plt.figure(figsize=(12, 8))
+# sns.heatmap(heatmap_data, annot=True, fmt=".2f")
+# plt.title('Heatmap of Movie Budget vs. Boxoffice')
+# plt.ylabel('Budget')
+# plt.xlabel('Boxoffice')
 # plt.show()
 
-# # 预算和票房的关系
-# sns.scatterplot(data=df_original, x='Budget max', y='Box office max', hue='Pays')
-# plt.show()
+
+
 
 # # 类别的条形图
 # sns.countplot(data=df_original, x='Language')
--- encode_qualitative.py
+++ encode_qualitative.py
@@ -1,12 +1,32 @@
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
+import numpy as np
 
 import data_utils as util
 
 ## exp for executing alone
-# file = 'data_2020-2024.tsv'
-# df = util.clean_data(pd.read_csv(file, sep='\t'))
+file = 'data/2020-2024_cleaned.tsv'
+df_cleaned = util.clean_data(pd.read_csv(file, sep='\t'),cols_retain=['Tconst du film','Box office max'])
+
+df_cleaned.rename(columns={'Tconst du film': 'tconst'}, inplace=True)
+df_title_basics = pd.read_csv('title.basics.tsv',sep='\t')
+df_title_basics = df_title_basics[['tconst','genres']]
+df_genres = util.join_data(dfs=[df_cleaned,df_title_basics],keys=['tconst'],manners=['left'])
+df_genres = util.clean_data(df_genres,cols_na=['genres'])
+df_genres.to_csv('genres.tsv',sep='\t')
+
+cnt_genres = dict() # count all distinct genres
+for s in df_genres['genres'].values:
+    vals = s.split(',')
+    for val in vals:
+        if val in cnt_genres:
+            cnt_genres[val] += 1
+        else: cnt_genres[val] = 1
+print(cnt_genres, len(cnt_genres))
+# {'Action': 244, 'Adventure': 189, 'Fantasy': 76, 'Comedy': 253, 'Drama': 503, 'Thriller': 122, 'Horror': 112,
+# 'Mystery': 50, 'Crime': 90, 'Romance': 85, 'War': 10, 'Musical': 18, 'Biography': 66, 'Animation': 87, 'Sci-Fi': 49,
+# 'Short': 191, 'Sport': 20, 'Family': 48, 'History': 50, 'Documentary': 36, 'Music': 21, '\\N': 2, 'Western': 1, 'Adult': 1}
 
 def encode_decomp(df,features,code='onehot'):
     """
@@ -14,15 +34,32 @@ def encode_decomp(df,features,code='onehot'):
         df: Dataframe, containing all original qualitative features
         features: list, quali features to be encoded
     """
-    # 假设df是您的DataFrame，"Pays"是需要进行one-hot编码的特征
-    encoder = OneHotEncoder(sparse=False)
-    feature_encoded = encoder.fit_transform(df[features])
+    # onehot encode
+    encoder = OneHotEncoder()#no sparse=False in new versions of scikit-learn
+    feature_encoded = encoder.fit_transform(pd.DataFrame(df[features]))
+    feature_encoded_dense = feature_encoded.toarray()# 转换为密集矩阵
 
-    # 使用PCA进行降维
-    pca = PCA(n_components=0.95)  # 保留95%的方差
+    # PCA for dim reduction
+    pca = PCA()  # n_components=2 # keep 2 main components as new features
+    X_new = pca.fit_transform(feature_encoded)
+    # if we want to keep some percent of variance
+    explained_variance_ratio_cumulative = np.cumsum(pca.explained_variance_ratio_)
+    threshold = 0.95 # 23 dim => 0.95:14, 0.80:9
+    n_components = np.argmax(explained_variance_ratio_cumulative >= threshold) + 1
+    pca = PCA(n_components=n_components)
     feature_reduced = pca.fit_transform(feature_encoded)
 
-    # 将降维后的数据转换回DataFrame，用于后续的分析或建模
-    df_feature_reduced = pd.DataFrame(feature_reduced)
+    # transform to dataframe after dim reduction
+    col_names = []
+    for i in range(n_components):
+        col_names.append('PC_genres_' + str(i))
+    df_feature_reduced = pd.DataFrame(feature_reduced,columns=col_names)
 
     return df_feature_reduced
+
+df_genres_pre_enc = df_genres.drop('genres',axis=1).join(df_genres['genres'].str.split(',').explode().reset_index(drop=True))
+df_genres_pre_enc.to_csv('df_genres_pre_enc.tsv',sep='\t')
+# print(df_genres_pre_enc.dtypes)
+# df_genres_encoded = pd.get_dummies(df_genres_pre_enc,columns=['genres'])
+df_genres_enc_decomp = encode_decomp(df_genres_pre_enc,'genres')
+df_genres_enc_decomp.to_csv('data/PCA/df_genres_enc_decomp.tsv',sep='\t')
